37d5ae08fb0ec277d80c84db51ebd9ffeaee86ca
IdayatSanni <idayatsanni@gmail.com>
2023-04-11 09:54:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// let firstName = prompt("What is your name");
+
+// if (firstName == "Idayat") {
+//   firstName = firstName.toLowerCase().trim();
+//   alert(`Welcome Idayat`);
+// } else if (firstName == undefined || firstName == "") {
+//   alert(`Your name is needed, reload your page`);
+// } else {
+//   alert(`Welcome ${firstName}`);
+// }
+
+// let index = 0;
+// while (index < 10) {
+//   index++;
+//   console.log(index);
+// }
